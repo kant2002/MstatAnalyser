2b775ba809add0fa64ee6ef23df314c514b7689e
Andrii Kurdiumov <kant2002@gmail.com>
2023-02-18 01:14:22 +0600

    Add parsing of some type mangling

---
+++ MstatAnalyser.Core.Tests/NodeConversionTests.cs
@@ -0,0 +1,61 @@
+﻿using Mono.Cecil;
+
+namespace MstatAnalyser.Core.Tests;
+
+[TestClass]
+public class NodeConversionTests
+{
+    [TestMethod]
+    public void TypeNodeVariant1()
+    {
+        var node = new Node(332, "Microsoft_AspNetCore_Mvc_Core__Module_");
+        NodeConverter converter = CreateTestConverter();
+        var regionNode = (TypeNode)converter.Convert(node);
+
+        Assert.AreEqual("<Module>", regionNode.TypeReference.FullName);
+    }
+    [TestMethod]
+    public void GenericTypeNode()
+    {
+        var node = new Node(332, "Microsoft_AspNetCore_Mvc_Core_Microsoft_Extensions_Internal_CopyOnWriteDictionaryHolder_2");
+        NodeConverter converter = CreateTestConverter();
+        var regionNode = (TypeNode)converter.Convert(node);
+
+        Assert.AreEqual("Microsoft.Extensions.Internal.CopyOnWriteDictionaryHolder`2", regionNode.TypeReference.FullName);
+    }
+    [TestMethod]
+    public void NestedTypeNode()
+    {
+        var node = new Node(332, "[Library]Library.Pages.Shared.Pages_Shared__Layout+<>c+<<ExecuteAsync>b__17_2>d");
+        NodeConverter converter = CreateTestConverter();
+        var regionNode = (TypeNode)converter.Convert(node);
+
+        Assert.AreEqual("Library.Pages.Shared.Pages_Shared__Layout/<>c/<<ExecuteAsync>b__17_2>d", regionNode.TypeReference.FullName);
+    }
+
+    private static NodeConverter CreateTestConverter()
+    {
+        var microsoftAspnetCoreAssembly = AssemblyDefinition.CreateAssembly(new AssemblyNameDefinition("Microsoft.AspNetCore.Mvc.Core", new()), "Microsoft.AspNetCore.Mvc.Core", ModuleKind.Dll);
+        var moduleType = new TypeDefinition("", "<Module>", TypeAttributes.Class);
+        microsoftAspnetCoreAssembly.Modules.First().Types.Add(moduleType);
+        var copyOnWriteDictionaryHolderType = new TypeDefinition("Microsoft.Extensions.Internal", "CopyOnWriteDictionaryHolder`2", TypeAttributes.Class);
+        copyOnWriteDictionaryHolderType.GenericParameters.Add(new GenericParameter("T1", copyOnWriteDictionaryHolderType));
+        copyOnWriteDictionaryHolderType.GenericParameters.Add(new GenericParameter("T2", copyOnWriteDictionaryHolderType));
+        microsoftAspnetCoreAssembly.Modules.First().Types.Add(copyOnWriteDictionaryHolderType);
+        
+        var libraryAssembly = AssemblyDefinition.CreateAssembly(new AssemblyNameDefinition("Library", new()), "Library", ModuleKind.Dll);
+        var pageLayoutType = new TypeDefinition("Library.Pages.Shared", "Pages_Shared__Layout", TypeAttributes.Class);
+        var pageLayoutCType = new TypeDefinition("", "<>c", TypeAttributes.Class);
+        pageLayoutType.NestedTypes.Add(pageLayoutCType);
+        var pageLayoutCExecuteAsyncType = new TypeDefinition("", "<<ExecuteAsync>b__17_2>d", TypeAttributes.Class);
+        pageLayoutCType.NestedTypes.Add(pageLayoutCExecuteAsyncType);
+        libraryAssembly.Modules.First().Types.Add(pageLayoutType);
+        var converter = new NodeConverter(new[]
+        {
+            moduleType,
+            copyOnWriteDictionaryHolderType,
+            pageLayoutType,
+        });
+        return converter;
+    }
+}
---
+++ MstatAnalyser.Core/NodeConverter.cs
@@ -0,0 +1,97 @@
+﻿using Mono.Cecil;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MstatAnalyser.Core;
+
+public class NodeConverter
+{
+    private readonly TypeReference[] types;
+
+    public NodeConverter(TypeReference[] types)
+    {
+        this.types = types;
+    }
+
+    public Node Convert(Node node)
+    {
+        foreach (var type in types)
+        {
+            if (TryConvertNode(type, node, out var convertedNode))
+            {
+                return convertedNode;
+            }
+        }
+
+        return node;
+    }
+
+    private bool TryConvertNode(TypeReference type, Node node, [NotNullWhen(true)]out Node? convertedNode)
+    {
+        var assemblyName = type.Scope.Name;
+        var typeName = type.FullName;
+        if (MangleAssemblyName(assemblyName.Replace(".", "_")) + "_" + MangleTypeName(typeName) == node.Name)
+        {
+            convertedNode = new TypeNode(node.Index, type);
+            return true;
+        }
+
+        if (MangleTypeName(typeName) == node.Name)
+        {
+            convertedNode = new TypeNode(node.Index, type);
+            return true;
+        }
+
+        if (typeName.Replace("/", "+") == node.Name)
+        {
+            convertedNode = new TypeNode(node.Index, type);
+            return true;
+        }
+
+        if ($"[{assemblyName}]{typeName.Replace("/", "+")}" == node.Name)
+        {
+            convertedNode = new TypeNode(node.Index, type);
+            return true;
+        }
+
+        if (typeName == node.Name)
+        {
+            convertedNode = new TypeNode(node.Index, type);
+            return true;
+        }
+
+        if (type is TypeDefinition typeDefinition)
+        {
+            foreach (var nestedType in typeDefinition.NestedTypes)
+            {
+                if (TryConvertNode(nestedType, node, out convertedNode))
+                {
+                    return true;
+                }
+            }
+        }
+
+        convertedNode = null;
+        return false;
+    }
+
+    private static string MangleTypeName(string originalType)
+    {
+        return originalType.Replace(".", "_").Replace("<", "_").Replace(">", "_").Replace("`", "_").Replace("/", "_");
+    }
+
+    private static string MangleAssemblyName(string originalAssembly)
+    {
+        return originalAssembly.Replace(".", "_");
+    }
+}
+
+public class TypeNode : Node
+{
+    internal TypeNode(int id, TypeReference typeReference)
+        : base(id, typeReference.FullName)
+    {
+        TypeReference = typeReference;
+    }
+
+    public TypeReference TypeReference { get; }
+}
